f221bd78e75e5f1d84daee28347eafccd2d1f9b5
ManoranjanMaharana1 <1705415@kiit.ac.in>
2020-12-19 11:35:03 +0530

    🎊Project Updated!!

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -1,23 +1,90 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Cell from './Cell';
 
 const Board = () => {
     const [curr, setCurr] = useState(true);
     const [o_arr, setOArray] = useState([]);
     const [x_arr, setXArray] = useState([]);
+    const [winner, setWinner] = useState(<></>);
+    const [gameover, setResults] = useState(false);
+    useEffect(() => {
+        if (!gameover) {
+            if ((o_arr.indexOf("1") !== -1 && o_arr.indexOf("2") !== -1 && o_arr.indexOf("3") !== -1)
+                || (o_arr.indexOf("4") !== -1 && o_arr.indexOf("5") !== -1 && o_arr.indexOf("6") !== -1)
+                || (o_arr.indexOf("7") !== -1 && o_arr.indexOf("8") !== -1 && o_arr.indexOf("9") !== -1)
+                || (o_arr.indexOf("1") !== -1 && o_arr.indexOf("4") !== -1 && o_arr.indexOf("7") !== -1)
+                || (o_arr.indexOf("2") !== -1 && o_arr.indexOf("5") !== -1 && o_arr.indexOf("8") !== -1)
+                || (o_arr.indexOf("3") !== -1 && o_arr.indexOf("6") !== -1 && o_arr.indexOf("9") !== -1)
+                || (o_arr.indexOf("1") !== -1 && o_arr.indexOf("5") !== -1 && o_arr.indexOf("9") !== -1)
+                || (o_arr.indexOf("3") !== -1 && o_arr.indexOf("5") !== -1 && o_arr.indexOf("7") !== -1)) {
+                console.log("o");
+                setResults(true);
+                setWinner(
+                    <div className="result">
+                        <h1>O WON!!</h1>
+                    </div>
+                )
+                freeze();
+            }
+            else if ((x_arr.indexOf("1") !== -1 && x_arr.indexOf("2") !== -1 && x_arr.indexOf("3") !== -1)
+                || (x_arr.indexOf("4") !== -1 && x_arr.indexOf("5") !== -1 && x_arr.indexOf("6") !== -1)
+                || (x_arr.indexOf("7") !== -1 && x_arr.indexOf("8") !== -1 && x_arr.indexOf("9") !== -1)
+                || (x_arr.indexOf("1") !== -1 && x_arr.indexOf("4") !== -1 && x_arr.indexOf("7") !== -1)
+                || (x_arr.indexOf("2") !== -1 && x_arr.indexOf("5") !== -1 && x_arr.indexOf("8") !== -1)
+                || (x_arr.indexOf("3") !== -1 && x_arr.indexOf("6") !== -1 && x_arr.indexOf("9") !== -1)
+                || (x_arr.indexOf("1") !== -1 && x_arr.indexOf("5") !== -1 && x_arr.indexOf("9") !== -1)
+                || (x_arr.indexOf("3") !== -1 && x_arr.indexOf("5") !== -1 && x_arr.indexOf("7") !== -1)) {
+                console.log("x");
+                setWinner(
+                    <div className="result">
+                        
+                        <h1>X WON!!</h1>
+                    </div>
+                )
+                setResults(true);
+                freeze();
+            }else if(o_arr.length + x_arr.length === 9){
+                setWinner(
+                    <div className="result">
+                        
+                        <h1>TIED!!</h1>
+                    </div>
+                )
+                setResults(true);
+            }
+        }
+    },[o_arr, x_arr, gameover])
+    const freeze = () => {
+        for (let i = 1; i <= 9; ++i) {
+            document.querySelector("#cell-container" + i.toString()).classList.add("disable");
+        }
+    }
+    const declareResult = (curr, idx) => {
+
+        setCurr(curr);
+
+        if (!curr) {
+            setOArray([...o_arr, idx.toString()]);
+        } else setXArray([...x_arr, idx.toString()]);
+
+    }
+
+
     return (
-        <div className="container">
-            <Cell obj={curr} setCurr={(curr) => setCurr(curr)} ckey="1" setOArray={(idx) => {if(idx !== -1) setOArray([...o_arr, idx])}} setXArray={(idx) => {if(idx !== -1) setXArray([...x_arr, idx])}}/>
-            <Cell obj={curr} setCurr={(curr) => setCurr(curr)} ckey="2" setOArray={(idx) => {if(idx !== -1) setOArray([...o_arr, idx])}} setXArray={(idx) => {if(idx !== -1) setXArray([...x_arr, idx])}}/>
-            <Cell obj={curr} setCurr={(curr) => setCurr(curr)} ckey="3" setOArray={(idx) => {if(idx !== -1) setOArray([...o_arr, idx])}} setXArray={(idx) => {if(idx !== -1) setXArray([...x_arr, idx])}}/>
-            <Cell obj={curr} setCurr={(curr) => setCurr(curr)} ckey="4" setOArray={(idx) => {if(idx !== -1) setOArray([...o_arr, idx])}} setXArray={(idx) => {if(idx !== -1) setXArray([...x_arr, idx])}}/>
-            <Cell obj={curr} setCurr={(curr) => setCurr(curr)} ckey="5" setOArray={(idx) => {if(idx !== -1) setOArray([...o_arr, idx])}} setXArray={(idx) => {if(idx !== -1) setXArray([...x_arr, idx])}}/>
-            <Cell obj={curr} setCurr={(curr) => setCurr(curr)} ckey="6" setOArray={(idx) => {if(idx !== -1) setOArray([...o_arr, idx])}} setXArray={(idx) => {if(idx !== -1) setXArray([...x_arr, idx])}}/>
-            <Cell obj={curr} setCurr={(curr) => setCurr(curr)} ckey="7" setOArray={(idx) => {if(idx !== -1) setOArray([...o_arr, idx])}} setXArray={(idx) => {if(idx !== -1) setXArray([...x_arr, idx])}}/>
-            <Cell obj={curr} setCurr={(curr) => setCurr(curr)} ckey="8" setOArray={(idx) => {if(idx !== -1) setOArray([...o_arr, idx])}} setXArray={(idx) => {if(idx !== -1) setXArray([...x_arr, idx])}}/>
-            <Cell obj={curr} setCurr={(curr) => setCurr(curr)} ckey="9" setOArray={(idx) => {if(idx !== -1) setOArray([...o_arr, idx])}} setXArray={(idx) => {if(idx !== -1) setXArray([...x_arr, idx])}}/>
-            
-        </div>
+        <>
+            <div className="container">
+                <Cell obj={curr} setCurr={(curr, idx) => declareResult(curr, idx)} ckey="1" />
+                <Cell obj={curr} setCurr={(curr, idx) => declareResult(curr, idx)} ckey="2" />
+                <Cell obj={curr} setCurr={(curr, idx) => declareResult(curr, idx)} ckey="3" />
+                <Cell obj={curr} setCurr={(curr, idx) => declareResult(curr, idx)} ckey="4" />
+                <Cell obj={curr} setCurr={(curr, idx) => declareResult(curr, idx)} ckey="5" />
+                <Cell obj={curr} setCurr={(curr, idx) => declareResult(curr, idx)} ckey="6" />
+                <Cell obj={curr} setCurr={(curr, idx) => declareResult(curr, idx)} ckey="7" />
+                <Cell obj={curr} setCurr={(curr, idx) => declareResult(curr, idx)} ckey="8" />
+                <Cell obj={curr} setCurr={(curr, idx) => declareResult(curr, idx)} ckey="9" />
+            </div>
+            {winner}
+        </>
     )
 }
 export default Board;
--- src/Components/Cell.js
+++ src/Components/Cell.js
@@ -1,21 +1,19 @@
 import React, { useState } from 'react';
 
-const Cell = ({ obj, setCurr, ckey, setXArray, setOArray }) => {
+const Cell = ({ obj, setCurr, ckey}) => {
     const [imgSrc, setImgSrc] = useState("./rec.svg");
     const enlargePic = () => {
 
         if (obj) {
             setImgSrc("./rec.svg");
-            setXArray(-1);
-            setOArray(ckey);
+            
         } else {
             setImgSrc("./delete.svg");
-            setOArray(-1);
-            setXArray(ckey);
+            
         }
         document.querySelector(".cell-" + ckey).classList.add("elementToAnimate");
         document.querySelector("#cell-container" + ckey).classList.add("disable");
-        setCurr(!obj)
+        setCurr(!obj, ckey);
     }
     return (
         <div className="cell-container" onClick={enlargePic} id={"cell-container" + ckey}>
